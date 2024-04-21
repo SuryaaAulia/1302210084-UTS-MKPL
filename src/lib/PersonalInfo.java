db2c1d8dc41e960b3ab9fccb5860cc6e56055df8
SuryaaAulia <suryaaulia489@gmail.com>
2024-04-21 16:44:09 +0700

    Menghapus variable yang tidak digunakan

--- src/lib/PersonalInfo.java
+++ src/lib/PersonalInfo.java
@@ -5,41 +5,23 @@ import java.util.List;
 
 public class PersonalInfo {
 
-    private String firstName;
-    private String lastName;
     private String idNumber;
-    private String address;
-
     private boolean isForeigner;
-    private boolean gender; // true = Laki-laki, false = Perempuan
-
-    private String spouseName;
     private String spouseIdNumber;
-
-    private List<String> childNames;
     private List<String> childIdNumbers;
 
-    public PersonalInfo(String firstName, String lastName, String idNumber, String address, String spouseName,
-            String spouseIdNumber, boolean isForeigner, boolean gender) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public PersonalInfo(String idNumber, String spouseIdNumber, boolean isForeigner) {
         this.idNumber = idNumber;
-        this.address = address;
         this.isForeigner = isForeigner;
-        this.gender = gender;
         this.spouseIdNumber = spouseIdNumber;
-        this.spouseName = spouseName;
-        childNames = new LinkedList<String>();
         childIdNumbers = new LinkedList<String>();
     }
 
     public void setSpouse(String spouseName, String spouseIdNumber) {
-        this.spouseName = spouseName;
         this.spouseIdNumber = idNumber;
     }
 
-    public void addChild(String childName, String childIdNumber) {
-        childNames.add(childName);
+    public void addChild(String childIdNumber) {
         childIdNumbers.add(childIdNumber);
     }
 
